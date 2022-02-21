90f8cf25f3e0d30cee521aa29f531f66eaa96089
siddhesh_prabhu <siddheshprabhu14@gmail.com>
2022-02-21 23:02:09 +0530

    Two player self rolling dice snake game

--- snake.c
+++ snake.c
@@ -1,14 +1,26 @@
+
 #include <stdio.h>
-int roll()
+char p1,p2;
+/*char check_name(char p)
 {
+    char p2;
+    printf("Enter initial character of Player 2 : ");
+    scanf("%c",&p2);
+    if(p==p2)
+    {
+        printf("Please pick another letter");
+        check_name(p);
+    }
+    return p2;
+}*/
+int roll(){
    return (rand()%5)+1;
 }
-int check(int s)
-{
+int check(int s, char p){
     if(s==100)
     {
         printf("\n==============================");
-        printf("\nYou are winner");
+        printf("\nPlayer %c is winner",p);
         printf("\nCongratulations\n");
         printf("==============================");
         return 1;
@@ -18,8 +30,7 @@ int check(int s)
         return 0;
     }
 }
-int check_step(int c)
-{
+int check_step(int c){
     switch(c)
     {
         case 1: return c+37;
@@ -40,32 +51,49 @@ int check_step(int c)
         default: return c;
     }
 }
-int main() {
-    // Write C code here
-    int r,step=0;
-    while(check(step)!=1)
-    {
-        printf("\nRoll Dice : ");
-        r=roll();
+int current_step(int r, int step, char p)
+{
         printf("%d",r);
         int current=step+r;
         if(step+r<101)
         {
-        printf("\nYou are currently at %d step.",current);
+        printf("\nPlayer %c is currently at %d step.",p,current);
         step=check_step(current);
         if(current<step)
         {
-            printf("\nYou climed ladder and now you are at %d step.",step);
+            printf("\nPlayer %c climed ladder and now at %d step.",p,step);
         }
         else if(current>step)
         {
-            printf("\nSnake ate you and now you are at %d step.",step);
+            printf("\nPlayer %c is eaten by snake and now you are at %d step.",p,step);
         }
         }
         else
         {
-            printf("\nRoll Again");
+            printf("\nRoll Again player %c",p);
         }
+        return step;
+}
+
+int main() {
+    int r,step1=0,step2=0;
+    char p1;
+    char p2='c';
+    printf("Welcome to snake-ladder game\nEnter the name of 2 players\n");
+    printf("Enter initial character of Player 1 : ");
+    scanf("%c",&p1);
+    //printf("Enter initial character of Player 2 : ");
+    //scanf("%c",&p2);
+    while(check(step1,p1)!=1 && check(step2,p2) !=1)
+    {
+        printf("\n-------------------\n");
+        //printf("%c : ",&p1);
+        printf("\nRoll Dice player %c: ",p1);
+        r=roll();
+        step1=current_step(r,step1, p1);
+        printf("\nRoll Dice player %c: ",p2);
+        r=roll();
+        step2=current_step(r,step2, p2);
     }
     return 0;
 }
